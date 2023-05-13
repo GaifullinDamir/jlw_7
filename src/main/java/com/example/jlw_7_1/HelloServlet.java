6c20e19693c048458ef2e354aef0ee330dc17a8c
Name <damiros.20002@gmail.com>
2023-05-14 01:34:13 +0300

    Is done

--- src/main/java/com/example/jlw_7_1/HelloServlet.java
+++ src/main/java/com/example/jlw_7_1/HelloServlet.java
@@ -1,31 +1,4 @@
 package com.example.jlw_7_1;
-//
-//import java.io.*;
-//
-//import jakarta.servlet.http.*;
-//import jakarta.servlet.annotation.*;
-//
-//@WebServlet(name = "helloServlet", value = "/hello-servlet")
-//public class HelloServlet extends HttpServlet {
-//    private String message;
-//
-//    public void init() {
-//        message = "Hello World!";
-//    }
-//
-//    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-//        response.setContentType("text/html");
-//
-//        // Hello
-//        PrintWriter out = response.getWriter();
-//        out.println("<html><body>");
-//        out.println("<h1>" + message + "</h1>");
-//        out.println("</body></html>");
-//    }
-//
-//    public void destroy() {
-//    }
-//}
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -34,40 +7,33 @@ import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 import jakarta.servlet.*;
 
-//@WebServlet("/hello")
-//public class HelloServlet extends HttpServlet {
-//
-//    @Override
-//    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        resp.setContentType("text/html");
-//        PrintWriter printWriter = resp.getWriter();
-//        printWriter.write("Hello!");
-//        printWriter.close();
-//    }
-//}
-
 @WebServlet(name = "ServletAppl", urlPatterns = {"/ServletAppl"}, loadOnStartup = 1)
 public class HelloServlet extends HttpServlet {
     static String ast;
-    static boolean b;
-    static long counter;
+    static boolean trigger;
     static int cycle;
 
-//    /**
-//     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-//     * methods.
-//     *
-//     * @param request servlet request
-//     * @param response servlet response
-//     * @throws ServletException if a servlet-specific error occurs
-//     * @throws IOException if an I/O error occurs
-//     */
+    static int[][] results;
 
     public HelloServlet(){
-        HelloServlet.ast = "a static var c=";
-        HelloServlet.b = false;
-        HelloServlet.counter = 0;
-        HelloServlet.cycle = 0;
+        HelloServlet.ast = "TRIGGER =";
+        HelloServlet.trigger = false;
+        HelloServlet.cycle = 6;
+    }
+
+    public int[] GetSumEvenOddNumbers(String args[]) {
+        int sumOddNumbers = 0; //переменная-счетчик для нечетных чисел
+        int sumEvenNumbers = 0; //переменная-счетчик для нечетных чисел
+        for(String strNum : args){
+            var intNum = Integer.parseInt(strNum);
+            if ( intNum % 2 == 0) {
+                sumEvenNumbers += intNum;
+            } else {
+                sumOddNumbers += intNum;
+            }
+        }
+
+        return new int[] {sumOddNumbers, sumEvenNumbers};
     }
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
@@ -78,83 +44,62 @@ public class HelloServlet extends HttpServlet {
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Здесь должны быть ФИО и № группы</title>");
+            out.println("<title>Гайфуллин, 4311</title>");
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>ServletAppl" + request.getServletPath() + "</h1>");
+            out.println("<h1> Лаб. раб 7</h1>");
+
             //=============I
 
-            if (HelloServlet.b) HelloServlet.b = false;
-            else HelloServlet.b = true;
-            HelloServlet.counter++;
-            if(HelloServlet.cycle < 5){
-                HelloServlet.cycle ++;
+            if (HelloServlet.trigger) HelloServlet.trigger = false;
+            else HelloServlet.trigger = true;
+
+
+            out.println("<h3> " + HelloServlet.ast + " " +HelloServlet.trigger + "</h3>");
+            out.println("<h3> Размер текста таблицы :" + HelloServlet.cycle + "</h3>");
+
+            String[] paramArr = request.getParameterValues("prmArr");
+            String prmSize = request.getParameter("size");
+            String fullName = request.getParameter("fname");
+            String groupNumber = request.getParameter("gnumber");
+
+            out.println("<h2> Студент: " + fullName + "</h2>");
+            out.println("<h2> Группа: " +  groupNumber + "</h2>");
+
+            if(prmSize != "") {
+                HelloServlet.cycle = Integer.parseInt(prmSize);
+            }
+
+            if(HelloServlet.cycle > 1){
+                HelloServlet.cycle --;
             }else{
-                HelloServlet.cycle = 1;
+                out.println("<h2>Дальнейшее увеличение не возможно</h2>");
             }
 
-            out.println("<h3> Servlet1.ast + Servlet1.b :" + HelloServlet.ast + HelloServlet.b + "</h3>");
-            out.println("<h3> Servlet1.counter : " + HelloServlet.counter + "</h3>");
-            out.println("<h3> Servlet1.cycle :" + HelloServlet.cycle + "</h3>");
+            int[] result = GetSumEvenOddNumbers(paramArr);
 
-            String[][] tbl = new String[3][3];
-            for(int i=0; i<3; i++){
-                for(int j=0; j<3; j++){
-                    tbl[i][j] = Integer.toString(i+1) + "&" + Integer.toString(j+1);
-                }
-            }
-
-            String prm1 = request.getParameter("prm1");
-            String prm2 = request.getParameter("prm2");
-            String prm3 = request.getParameter("prm3");
-
-
-            out.println("<h" + HelloServlet.cycle + "><table border>"+
-                    "<tr>" + "<td>" + tbl[0][0] + "</td>" + "<td>prm1=" + prm1 + "</td>" + "<td>1.3</td>" +
-                    "<tr>" + "<td>prm2=" + prm2 + "</td>" + "<td>pr2</td>" + "<td>2.3</td>" +
-                    "<tr>" + "<td>prm3=" + prm3 + "</td>" + "<td>3.2</td>" + "<td>" + tbl[2][2] + "</td>" +
-                    "</tr>"
-                    + "</table></h" + HelloServlet.cycle + ">");
-
-            //=============II
+            out.println("<h" + HelloServlet.cycle + ">" +
+                    "<tr>" + "<td>" + "Sum of odd numbers: " + result[0] +
+                            " Sum of even numbers: " + result[1] + "</td>" + "</tr>" +
+                    "</table></h" + HelloServlet.cycle + ">");
             out.println("</body>");
             out.println("</html>");
         }
     }
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
+
     @Override
     public String getServletInfo() {
         return "Short description";
-    }// </editor-fold>
+    }
 }
